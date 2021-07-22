20cbfaf43b2d0731c96f1db71c4fd7bdfb5bf233
Jae Won Jang <jwjang@elground.com>
2021-07-22 17:19:13 +0900

    gatsby bootstrap, includes lint and essential plugins

---
+++ app/gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Behavior Data Pipeline Demo App",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-netlify",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "assets/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./assets/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
---
+++ app/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Root = () => {
+  return (
+    <div>
+      Hello World!
+    </div>
+  )
+}
+
+export default Root
