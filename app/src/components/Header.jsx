a56cd5c7708ceade1b509adf697662372073c710
Ayan Mukanov <mukanov.ayan@gmail.com>
2021-08-15 19:51:23 +0900

    style: change header svg and elements size

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -11,19 +11,25 @@ function Header({ location }) {
     <header className="px-6 container mx-auto bg-white w-full max-w-screen-xl flex-grow flex items-center justify-between">
       <div className="pt-6 w-full">
         <nav className="flex items-center justify-between flex-wrap">
-          <svg className="fill-current text-primary w-5" viewBox="0 0 26 44" xmlns="http://www.w3.org/2000/svg">
-            <path
-              clipRule="evenodd"
-              d="m20.7997 5.86658-5.1998 2.9335-5.1998 2.93312-5.20027 2.9335v5.8665 2.9336 2.933 2.9335 2.9335l5.20027-2.9335 5.1998-2.9335v-2.933-2.9336l-5.1998 2.9336v-5.8666l5.1998-2.9335 5.1998-2.9335v2.9335 2.9335 2.933 2.9336 2.933 2.9335l-5.1998 2.9335-5.1998 2.9331-5.20027 2.9335-5.19983 2.9331 5.19983 2.9335 5.20027-2.9335 5.1998-2.9331 5.1998-2.9335 5.2003-2.9331v-5.867-5.8666-5.8665-2.2501-3.61652-5.86658l-5.2003-2.9335z"
-              fillRule="evenodd"
-            />
-          </svg>
-
+          <Link to="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8 fill-current text-primary"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Link>
           <ul className="hidden md:mx-auto md:block md:flex-grow md:ml-4">
             <li className="block my-4 md:inline-block md:my-0">
               <Link
                 to="/"
-                className="text-lightgray hover:text-slategray hover:bg-gainsboro rounded-full py-2 px-3 font-medium"
+                className="text-lightgray hover:text-slategray hover:bg-gainsboro rounded-full py-2 px-3 font-medium text-lg"
               >
                 All
               </Link>
@@ -31,7 +37,7 @@ function Header({ location }) {
             {categories.map((category) => (
               <li key={category.id} className="block my-4 md:inline-block md:my-0">
                 <Link
-                  className="text-lightgray hover:text-slategray hover:bg-gainsboro rounded-full py-2 px-3 font-medium"
+                  className="text-lightgray hover:text-slategray hover:bg-gainsboro rounded-full py-2 px-3 font-medium text-lg"
                   to={`/categories?id=${category.id}`}
                 >
                   {category.name}
@@ -43,7 +49,7 @@ function Header({ location }) {
           <div className="flex items-center">
             <div className="ml-8">
               <Link to="/search" className="text-slategray">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className="fill-current w-4">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className="w-6 fill-current">
                   <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
                 </svg>
               </Link>
@@ -56,7 +62,7 @@ function Header({ location }) {
                 )}
 
                 <span className="text-slategray">
-                  <svg className="fill-current w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <svg className="fill-current w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path d="M4 2h16l-3 9H4a1 1 0 1 0 0 2h13v2H4a3 3 0 0 1 0-6h.33L3 5 2 2H0V0h3a1 1 0 0 1 1 1v1zm1 18a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm10 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" />
                   </svg>
                 </span>
--- app/src/components/Product.jsx
+++ app/src/components/Product.jsx
@@ -8,7 +8,7 @@ const Product = ({ id, name, brand, category, type, sizes, price, quantity, colo
   return (
     <article key={id} className="p-6 w-full md:w-1/2 lg:w-1/3">
       <Link to={`/products?id=${id}`} className="group no-underline w-full h-full flex">
-        <div className="bg-gainsboro rounded-lg cursor-pointer w-full overflow-hidden relative px-3 py-6 md:px-6 hover:bg-opacity-75">
+        <div className="bg-gainsboro rounded-lg cursor-pointer w-full overflow-hidden relative px-3 py-6 md:px-6 hover:opacity-90">
           <img src={imgUrl} alt={name} title={name} className="max-w-full h-auto align-middle" />
           <div className="pt-3 md:pt-6 text-center">
             <p className="text-slategray font-bold text-lg group-hover:text-primary mb-1">{name}</p>
